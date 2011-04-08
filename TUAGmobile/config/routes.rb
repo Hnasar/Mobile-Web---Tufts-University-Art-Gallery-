5b9f1081775f2500dd4b45602a9e80e16decedd9
Rafi <ryagudin@gmail.com>
2011-04-08 01:31:59 -0400

    Made add jQuery footer / header. Tested out some buttons.

--- TUAGmobile/config/routes.rb
+++ TUAGmobile/config/routes.rb
@@ -50,6 +50,7 @@ TUAGmobile::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+  # route to the home view
   root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
