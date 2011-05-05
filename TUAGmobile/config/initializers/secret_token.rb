4b3290bb6933fe05d5164abf0cc8d3549fee205c
makala <makala.noble@gmail.com>
2011-05-04 22:48:29 -0400

    one folder to rule them all

--- TUAGmobile/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-TUAGmobile::Application.config.secret_token = '5916ec32e9c2cee6cd06d182cb503de41b90af87c0efe67f4f41d48539b82f56014e5368f21ba5e90ad0fda2352db0465dbad9306c8a9e673fd30c04682620f7'
