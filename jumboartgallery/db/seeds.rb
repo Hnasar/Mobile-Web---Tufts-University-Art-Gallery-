ebc6cd0ab9c89d9e8dbf32cfd00941832a71a756
hnasar <Hashem.nasarat@tufts.edu>
2011-05-06 23:38:00 -0400

    implemented slideshow on homepage

--- jumboartgallery/db/seeds.rb
+++ jumboartgallery/db/seeds.rb
@@ -1,7 +1,7 @@
 require 'csv'
 
 Artwork.delete_all
-FasterCSV.foreach("lib/Collection_results.csv", :headers => true) do |row|
+CSV.foreach("lib/Collection_results.csv", :headers => true) do |row|
   Artwork.create(:filename => row[0], :object => row[1], :creator => row[2], :title => row[3], :description => row[4], :location => row[5], :recfrom => row[6], :date => row[7])
 end
 
